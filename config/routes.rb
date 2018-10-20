210fe1eb9f9cdb16c8179589a7e5c1b19fea6b73
barlima <bartekperucki@gmail.com>
2018-10-20 11:17:42 +0200

    Clean up

--- config/routes.rb
+++ config/routes.rb
@@ -2,8 +2,7 @@ Rails.application.routes.draw do
 
   namespace :api do
     namespace :v1 do
-      resources :ideas  
-      
+      # Users
       post 'user_token' => 'user_token#create'
 
       get    '/users'          => 'users#index'
